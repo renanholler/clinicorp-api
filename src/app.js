196e61957f535db5e358a1b71e6596533a23f241
Renan Holler <rrholler@gmail.com>
2024-10-20 21:36:06 -0300

    fix: cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Rotas
